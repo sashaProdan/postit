c41acc94980bf2da46daba49adee96e088afb9e4
Sasha Prodan <prodan.sasha@gmail.com>
2018-12-27 16:39:38 -0600

    add create post form with strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.first # TODO: change once we have authentication
+
+    if @post.save
+      flash[:notice] = "your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
   end
 end
