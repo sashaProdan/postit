8ebd419cbbfa4ff88113db2aeb26f87af466fa8c
Sasha Prodan <prodan.sasha@gmail.com>
2018-12-28 21:59:54 -0600

    add user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
-
+  
+  get '/register', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
@@ -9,4 +10,6 @@ PostitTemplate::Application.routes.draw do
     resources :comments, only: [:create]
   end
   resources :categories, only: [:new, :create, :show]
+
+  resources :users, only: [:show, :create, :edit, :update]
 end
