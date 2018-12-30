eebd9850e5e59ba54866a2f8b911f4138462490a
Sasha Prodan <prodan.sasha@gmail.com>
2018-12-29 18:36:34 -0600

    add voting for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:show, :index]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by{|x| x.total_votes}.reverse
   end
 
   def show
@@ -19,8 +19,8 @@ class PostsController < ApplicationController
     @post.creator = current_user
 
     if @post.save
-      flash[:notice] = "your post was created."
-      redirect_to posts_path
+      flash[:notice] = "Your post was created."
+      redirect_to :back
     else
       render :new
     end
@@ -35,6 +35,12 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def vote
+    Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+    flash[:notice] = "Your vote was counted."
+    redirect_to posts_path
   end
 
   private
