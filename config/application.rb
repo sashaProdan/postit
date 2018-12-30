02060c77dac101cb669ba988ee25c2ea80f8da4f
Sasha Prodan <prodan.sasha@gmail.com>
2018-12-29 20:58:28 -0600

    add precompiling configuration

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module PostitTemplate
 
     # Tealeaf note: Bootstrap sass gem addition
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.initialize_on_precompile = false
   end
 end
